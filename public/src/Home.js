07c6a7163e8361f04a0cbb0ce5fb7ea736954f69
Nyan Min Htet <niallhtet@gmail.com>
2022-01-12 15:01:03 -0800

    added Myanmar section

--- public/src/Home.js
+++ public/src/Home.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./index.css";
+import useIntersection from "./components/Helpers/Helpers.js";
 import Landing from "./components/Home/Landing/Landing";
 import Introduction from "./components/Home/Introduction/Introduction";
 import MyStory from "./components/Home/MyStory/MyStory";
 import Myanmar from "./components/Home/Myanmar/Myanmar";
 
 function Home() {
+    const [offsetY, setOffsetY] = useState(0);
+
+    const handleScroll = () => {
+        setOffsetY(window.pageYOffset);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className="homepage">
             <Landing />
--- public/src/components/Home/Myanmar/Myanmar.js
+++ public/src/components/Home/Myanmar/Myanmar.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Myanmar.module.css";
+import { Parallax } from "react-parallax";
+import backgroundImage from "./Myanmar.jpg";
+import hotAirBalloon from "./HotAirBalloon.png";
 
-function Myanmar() {
+const Myanmar = () => {
+    const [offsetY, setOffsetY] = useState(0);
+
+    const handleScroll = () => {
+        setOffsetY(window.pageYOffset);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+    
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <Parallax
+                className={styles.background}
+                bgImage={backgroundImage}
+                alt="myanmar"
+                strength={700}
+            ></Parallax>
+            <img
+                src={hotAirBalloon}
+                alt="hotairballoon"
+                className={styles["hot-air-balloon-3"]}
+                style={{ transform: `translateX(${offsetY * -0.2}px)` }}
+            ></img>
+            <img
+                src={hotAirBalloon}
+                alt="hotairballoon"
+                className={styles["hot-air-balloon-2"]}
+                style={{ transform: `translateX(${offsetY * -0.6}px)` }}
+            ></img>
+            <img
+                src={hotAirBalloon}
+                alt="hotairballoon"
+                className={styles["hot-air-balloon-1"]}
+                style={{ transform: `translateX(${offsetY * -1}px)` }}
+            ></img>
+            <div
+                className={styles.biography}
+                style={{ transform: `translateY(${offsetY * -0.5}px)` }}
+            >
+                I was born and raised in Myanmar, a country enriched with
+                culture and beauty the world has yet to see.<br></br>
+                <br></br>
+                She was hidden among the mountains, and the historical relics,
+                and one brutal, brutal dictatorship which continues to this day.
+                <br></br>
+                <br></br>As exquisite as Myanmar was, she was also
+                technologically regressive, thanks to a power-hungry military.
+                <br></br>
+                <br></br>Growing up, I was exposed to very little technology. I
+                nearly majored in the humanities for college. But fate had other
+                plans. At age 16, I discovered coding.
+            </div>
+        </div>
     );
-}
+};
 
 export default Myanmar;
