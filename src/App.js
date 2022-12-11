4507add89ef84e27698cbe55027f34726629ec5c
Niall Htet <niallhtet@Nialls-MBP.lan>
2022-12-10 19:37:15 -0800

    Upgraded react-scripts version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyStory from "./components/MyStory/MyStory";
 import Main from "./components/Home/Main";
 import Projects from "./components/Projects/Projects";
@@ -7,7 +7,7 @@ import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<MyStory />} />
         <Route path="/portfolio-website" element={<MyStory />} />
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
-    </HashRouter>
+    </Router>
   );
 };
 
